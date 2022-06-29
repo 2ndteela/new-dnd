0e4d080049d498b166ff07b2d959219f2c238bc8
Jeremy Teela <2ndteela@gmail.com>
2022-06-29 12:23:10 -0600

    adding nifty transistions

--- src/components/MultiPannelViewer/MutliPannelViewer.js
+++ src/components/MultiPannelViewer/MutliPannelViewer.js
@@ -9,27 +9,43 @@ import SideNav from "../SideNav/SideNav"
 import { writeCharacterToDb } from "../../assets/services"
 
 export default function MultiPannelViewer() {
-    const [ pannels, setPannels ] = useState(['skills', 'fight'])
     const windowWidth = window.innerWidth
+    const [ pannelOne, setPannelOne ] = useState('skills')
+    const [ pannelTwo, setPannelTwo ] = useState(windowWidth > 1099 ? 'fight' : null)
+    const [ pannelToAdd, setPannelToAdd ] = useState(null)
+    const [ transitionActive, setTransitionActive ] = useState(true)
 
     function getPannel(num) {
 
-
         let header = 'Fight'
         let body = <Fight />
-        if(pannels[num] === 'pack') {
+        let toGet = pannelOne
+
+
+        if(num === -1) {
+            if(!pannelToAdd)
+                return null
+
+            toGet = pannelToAdd
+        }
+
+        else if(num === 1)
+            toGet = pannelTwo
+        
+        
+        if(toGet === 'pack') {
             header = "Pack"
             body = <Pack />
         }
-        else if (pannels[num] === 'skills') {
+        else if (toGet === 'skills') {
             header = 'Skills'
             body = <Skills />
         }
-        else if(pannels[num] === 'edit') {
+        else if(toGet === 'edit') {
             header = 'Edit'
             body = <Edit />
         }
-        else if(pannels[num] === 'spells') {
+        else if(toGet === 'spells') {
             header = 'Spells'
             body = <Spells />
         }
@@ -47,24 +63,41 @@ export default function MultiPannelViewer() {
     }
 
     function addPannel(p) {
-        if(pannels[0] !== p && pannels[1] !== p) {
-            const ps = [...pannels]
-            ps[1] = ps[0]
-            ps[0] = p
 
-            setPannels(ps)
-            writeCharacterToDb()
-        }
+        setPannelToAdd(p)
+        setTransitionActive(true)
+
+        setTimeout(() => {
+            if(pannelOne !== p && pannelTwo !== p) {
+                if(windowWidth > 1099)
+                    setPannelTwo(pannelOne + '')
+                
+                setPannelOne(p)
+                writeCharacterToDb()
+            }
+            setPannelToAdd(null)
+            setTransitionActive(false)
+        }, 500)
     }
 
     return (
         <div id="pannels-container">
             <div>
-                <SideNav addPannel={addPannel} pannels={pannels} />
+                <SideNav addPannel={addPannel} pannels={[pannelOne, pannelTwo]} />
             </div>
             <div id="all-the-pannels" >
-                <div className="pannel-in-viewer"> {getPannel(0)}</div>
-                {windowWidth > 1100 && <div className="pannel-in-viewer second-pannel"> {getPannel(1)}</div>}
+                <div id="pannel-window">
+                    <div id="area-to-add-new-window" className={`${pannelToAdd !== null ? 'add-in' : ''} ${transitionActive ? 'in-transition' : ''}`}>
+                        {getPannel(-1)}
+                    </div>
+                    <div 
+                        className={`pannel-in-viewer ${pannelToAdd !== null ? 'move-right' : ''} ${transitionActive ? 'in-transition' : ''}`} 
+                    > {getPannel(0)}</div>
+                    {
+                    windowWidth > 1100 && 
+                    <div className={`pannel-in-viewer second-pannel ${pannelToAdd !== null ? 'move-right fade-out' : ''} ${transitionActive ? 'in-transition' : ''}`}> {getPannel(1)}</div>
+                    }
+                </div>
             </div>
         </div>
     )
--- src/components/SideNav/SideNav.js
+++ src/components/SideNav/SideNav.js
@@ -3,15 +3,34 @@ import './SideNav.css'
 import { GiCrossedSwords, GiSkills, GiLightBackpack, GiSpellBook} from "react-icons/gi";
 import { FaEdit, FaSignOutAlt } from 'react-icons/fa'
 import { useNavigate } from 'react-router-dom';
+import { loadCharacter } from '../../assets/utilities';
 
 
 export default function SideNav({addPannel, pannels}) {
 
+    const windowWidth = window.innerWidth
+    const character = loadCharacter()
+
+    let buttons = [
+        {name: 'fight', icon: <GiCrossedSwords />},
+        {name: 'pack', icon: <GiLightBackpack />}, 
+        {name: 'skills', icon: <GiSkills />}, 
+        {name: 'spells', icon: <GiSpellBook />}, 
+        {name: 'edit', icon: <FaEdit />}
+    ]
+
+    if(!character?.spells) {
+        buttons = [
+            {name: 'fight', icon: <GiCrossedSwords />},
+            {name: 'pack', icon: <GiLightBackpack />}, 
+            {name: 'skills', icon: <GiSkills />}, 
+            {name: 'edit', icon: <FaEdit />}
+        ]
+    }
+
     const navigate = useNavigate()  
 
     function makeStyles(which) {
-
-        const windowWidth = window.innerWidth
 
         if(pannels[0] === which) 
             return {
@@ -31,15 +50,10 @@ export default function SideNav({addPannel, pannels}) {
     return (
         <div id='side-nav-container'>
             <div style={{width: '100%'}} id="side-nav-buttons" >
-                <button onClick={() => addPannel('fight')} style={makeStyles('fight')} ><GiCrossedSwords></GiCrossedSwords></button>
-                <button onClick={() => addPannel('pack')} style={makeStyles('pack')}><GiLightBackpack></GiLightBackpack></button>
-                <button onClick={() => addPannel('skills')} style={makeStyles('skills')}><GiSkills></GiSkills></button>
-                <button onClick={() => addPannel('spells')} style={makeStyles('spells')}><GiSpellBook></GiSpellBook></button>
-                <button onClick={() => addPannel('edit')} style={makeStyles('edit')}><FaEdit></FaEdit></button>
+                {buttons.map(b => <button onClick={() => addPannel(b.name)} style={makeStyles(b.name)}>{b.icon}</button> )}
+                { windowWidth < 1099 && <button id="sign-out-button" onClick={() => navigate('/characters')} > <FaSignOutAlt></FaSignOutAlt></button>}
             </div>
-            <div>
-                <button id="sign-out-button" onClick={() => navigate('/characters')} > <FaSignOutAlt></FaSignOutAlt></button>
-            </div>
+            { windowWidth > 1099 && <button id="sign-out-button" onClick={() => navigate('/characters')} > <FaSignOutAlt></FaSignOutAlt></button>}
         </div>
     )
 }
